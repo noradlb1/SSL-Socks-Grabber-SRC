6c2c2ea48b10587f4a3b90c6255c037bdebbb330
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-27 05:54:38 +0100

    SSL-Socks-Grabber-SRC
    SSL-Socks-Grabber-SRC

---
+++ SSLProxy Grabber/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.LinkLabel5 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel4 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(4, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(401, 443)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button3
+        '
+        Me.Button3.FlatAppearance.BorderSize = 2
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button3.Location = New System.Drawing.Point(6, 198)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(170, 43)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label4.Font = New System.Drawing.Font("Verdana", 13.75!, System.Drawing.FontStyle.Bold)
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.Location = New System.Drawing.Point(353, 3)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 23)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "?"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.LinkLabel5)
+        Me.Panel2.Controls.Add(Me.LinkLabel3)
+        Me.Panel2.Controls.Add(Me.LinkLabel4)
+        Me.Panel2.Controls.Add(Me.LinkLabel2)
+        Me.Panel2.Controls.Add(Me.LinkLabel1)
+        Me.Panel2.Location = New System.Drawing.Point(6, 249)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(170, 169)
+        Me.Panel2.TabIndex = 8
+        '
+        'LinkLabel5
+        '
+        Me.LinkLabel5.Font = New System.Drawing.Font("Verdana", 9.0!)
+        Me.LinkLabel5.Location = New System.Drawing.Point(9, 134)
+        Me.LinkLabel5.Name = "LinkLabel5"
+        Me.LinkLabel5.Size = New System.Drawing.Size(155, 28)
+        Me.LinkLabel5.TabIndex = 4
+        Me.LinkLabel5.TabStop = True
+        Me.LinkLabel5.Text = "http://live-socks.net/" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "feeds/posts/default"
+        Me.LinkLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.Font = New System.Drawing.Font("Verdana", 9.0!)
+        Me.LinkLabel3.Location = New System.Drawing.Point(9, 79)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(155, 49)
+        Me.LinkLabel3.TabIndex = 3
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "https://my-proxy.com/" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "free-socks-5-proxy.html"
+        Me.LinkLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LinkLabel4
+        '
+        Me.LinkLabel4.AutoSize = True
+        Me.LinkLabel4.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel4.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel4.Location = New System.Drawing.Point(24, 66)
+        Me.LinkLabel4.Name = "LinkLabel4"
+        Me.LinkLabel4.Size = New System.Drawing.Size(121, 14)
+        Me.LinkLabel4.TabIndex = 2
+        Me.LinkLabel4.TabStop = True
+        Me.LinkLabel4.Text = "SOCKS from sites:"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Font = New System.Drawing.Font("Verdana", 9.0!)
+        Me.LinkLabel2.Location = New System.Drawing.Point(13, 39)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(143, 14)
+        Me.LinkLabel2.TabIndex = 1
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "https://sslproxies.org"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel1.Location = New System.Drawing.Point(29, 9)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(110, 14)
+        Me.LinkLabel1.TabIndex = 0
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "HTTPS from site:"
+        '
+        'Button4
+        '
+        Me.Button4.FlatAppearance.BorderSize = 2
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button4.Location = New System.Drawing.Point(6, 149)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(170, 43)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Save"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderSize = 2
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button2.Location = New System.Drawing.Point(6, 100)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(170, 43)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Grab SOCKS"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderSize = 2
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(6, 50)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(170, 43)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Grab HTTPS"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Verdana", 8.75!, System.Drawing.FontStyle.Bold)
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(184, 423)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 14)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Idle..."
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.Black
+        Me.ListBox1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 14
+        Me.ListBox1.Location = New System.Drawing.Point(182, 50)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(208, 368)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 13.75!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(375, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 23)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "X"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(3, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(256, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SSL && Socks Grabber"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(408, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SSLProxy Grabber"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents LinkLabel4 As LinkLabel
+    Friend WithEvents LinkLabel5 As LinkLabel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ SSLProxy Grabber/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.Drawing
+Imports System.IO
+Imports System.Linq
+Imports System.Net
+Imports System.Runtime.CompilerServices
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms
+Imports HtmlAgilityPack
+Public Class Form1
+    Private dragging As Boolean
+    Private offset As Point
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        Me.dragging = True
+        Me.offset = e.Location
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If Me.dragging Then
+            Dim point As Point = MyBase.PointToScreen(e.Location)
+            MyBase.Location = New Point((point.X - Me.offset.X), (point.Y - Me.offset.Y))
+            Cursor.Current = Cursors.NoMove2D
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        Me.dragging = False
+    End Sub
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim CP As CreateParams = MyBase.CreateParams
+            CP.Style = &HA0000
+            Return CP
+        End Get
+    End Property
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Me.ListBox1.Items.Clear()
+            Dim htmlWeb As HtmlWeb = New HtmlWeb()
+            Dim htmlDocument As HtmlAgilityPack.HtmlDocument = New HtmlAgilityPack.HtmlDocument()
+            htmlDocument = htmlWeb.Load("https://www.sslproxies.org")
+            Dim htmlNode As HtmlNode = htmlDocument.DocumentNode.Descendants("tbody").FirstOrDefault()
+            Dim enumerable As IEnumerable(Of HtmlNode) = htmlNode.Descendants("tr")
+            For Each htmlNode2 As HtmlNode In enumerable
+                Dim text As String = htmlNode2.FirstChild.InnerText
+                Dim innerText As String = htmlNode2.SelectSingleNode("td[2]").InnerText
+                text = text + ":" + innerText
+                Me.ListBox1.Items.Add(text)
+            Next
+            Me.Label3.Text = Me.ListBox1.Items.Count.ToString()
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.ListBox1.Items.Clear()
+        Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create("https://www.my-proxy.com/free-socks-5-proxy.html"), HttpWebRequest)
+        Dim httpWebResponse As HttpWebResponse = CType(httpWebRequest.GetResponse(), HttpWebResponse)
+        Dim streamReader As StreamReader = New StreamReader(httpWebResponse.GetResponseStream())
+        Dim input As String = streamReader.ReadToEnd()
+        Dim regex As Regex = New Regex("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}")
+        Dim matchCollection As MatchCollection = regex.Matches(input)
+        Try
+            For Each obj As Object In matchCollection
+                Dim item As Match = CType(obj, Match)
+                Me.ListBox1.Items.Add(item)
+            Next
+        Finally
+        End Try
+        Dim httpWebRequest2 As HttpWebRequest = CType(WebRequest.Create("http://www.live-socks.net/feeds/posts/default"), HttpWebRequest)
+        Dim httpWebResponse2 As HttpWebResponse = CType(httpWebRequest2.GetResponse(), HttpWebResponse)
+        Dim streamReader2 As StreamReader = New StreamReader(httpWebResponse2.GetResponseStream())
+        Dim input2 As String = streamReader2.ReadToEnd()
+        Dim regex2 As Regex = New Regex("[0-9].{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}")
+        Dim matchCollection2 As MatchCollection = regex2.Matches(input2)
+        Try
+            For Each obj2 As Object In matchCollection2
+                Dim item2 As Match = CType(obj2, Match)
+                Me.ListBox1.Items.Add(item2)
+            Next
+        Finally
+        End Try
+        Me.Label3.Text = Me.ListBox1.Items.Count.ToString()
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://www.sslproxies.org")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Process.Start("https://my-proxy.com/free-socks-5-proxy.html")
+    End Sub
+
+    Private Sub LinkLabel5_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel5.LinkClicked
+        Process.Start("http://www.live-socks.net/feeds/posts/default")
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        MsgBox("Samad.Dz!!")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Me.ListBox1.Items.Clear()
+            Me.Label3.Text = "Idle..."
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            If ListBox1.Items.Count <= 0 Then
+                MsgBox("Grab Proxies first and save", vbInformation, "INfo")
+            Else
+                Dim array As String() = New String() {Me.ListBox1.Items.ToString()}
+                Dim saveFileDialog As SaveFileDialog = New SaveFileDialog()
+                saveFileDialog.Filter = "Proxy-List (*.txt)|*.txt|All Files (*.*)|*.*"
+                saveFileDialog.CheckPathExists = True
+                saveFileDialog.ShowDialog(Me)
+                Dim streamWriter As StreamWriter = New StreamWriter(saveFileDialog.FileName)
+                Dim num As Integer = Me.ListBox1.Items.Count - 1
+                For i As Integer = 0 To num
+                    streamWriter.WriteLine(RuntimeHelpers.GetObjectValue(Me.ListBox1.Items(i)))
+                Next
+                streamWriter.Close()
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
